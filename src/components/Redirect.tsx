47ff9075ee34fd81004901b4ba45c2b21b835528
Thor Amorim <thoramorim@gmail.com>
2018-03-20 00:31:52 -0300

    Add Redirect component

---
+++ src/components/Redirect.tsx
@@ -0,0 +1,22 @@
+import { maybe } from 'tsmonad'
+import { Component } from 'preact'
+import { route } from 'preact-router'
+
+interface Props {
+  to: string,
+  onEnter?: Function
+}
+
+class Redirect extends Component<Props, {}> {
+  componentWillMount() {
+    maybe(this.props.onEnter as Function)
+      .map(onEnter => onEnter())
+    route(this.props.to, true)
+  }
+
+  render() {
+    return null
+  }
+}
+
+export default Redirect
