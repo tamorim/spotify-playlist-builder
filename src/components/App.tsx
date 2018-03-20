4b9c9d3198ad46d2e3625ab40b36e23e0477efef
Thor Amorim <thoramorim@gmail.com>
2018-03-20 00:45:26 -0300

    Show playlists titles on App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,10 +3,13 @@ import { maybe } from 'tsmonad'
 import { connect } from 'unistore/preact'
 
 import { AUTH_URL } from '../utils/constants'
+import { actions, Playlists } from '../store/playlists'
 import { Authentication } from '../store/authentication'
 
 interface Props {
-  authentication: Authentication
+  playlists: Playlists,
+  authentication: Authentication,
+  getPlaylists: (authorization: string) => void
 }
 
 const App = (props: Props) => {
@@ -18,12 +21,26 @@ const App = (props: Props) => {
     <div>
       <h1>Playlists</h1>
       { !authorization && <a href={AUTH_URL}>Login</a> }
+      {
+        authorization && (
+          maybe(props.playlists!)
+            .caseOf({
+              just: playlists =>
+                playlists.map(playlist => (
+                  <h2>{ playlist.name }</h2>
+                )),
+              nothing: () =>
+                props.getPlaylists(authorization)
+            })
+        )
+      }
     </div>
   )
 }
 
 const states = [
+  'playlists',
   'authentication'
 ]
 
-export default connect(states)(App)
+export default connect(states, actions)(App)
