e34dafe34b4213f6ebb796e45fd53d7d11223138
Thor Amorim <thoramorim@gmail.com>
2018-03-20 00:44:59 -0300

    Add playlists to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,14 @@
 import createStore from 'unistore'
 
+import playlists, { Playlists } from './playlists'
 import authentication, { Authentication } from './authentication'
 
 export interface Store {
+  playlists: Playlists,
   authentication: Authentication
 }
 
 export default createStore<Store>({
+  playlists,
   authentication
 })
---
+++ src/store/playlists.ts
@@ -0,0 +1,72 @@
+import { __, compose, assoc, prop } from 'ramda'
+
+import { Store } from './index'
+import { getPlaylists } from '../fetcher'
+
+export type Type = 'user' | 'playlist' | 'artist' | 'album'
+
+export interface Tracks {
+  href: string,
+  total: number
+}
+
+export interface Owner {
+  display_name: string,
+  external_urls: ExternalUrls,
+  href: string,
+  id: string,
+  type: Type,
+  uri: string
+}
+
+export interface Image {
+  width: number,
+  height: number,
+  url: string
+}
+
+export interface ExternalUrls {
+  [key: string]: string
+}
+
+export interface PlaylistItem {
+  collaborative: boolean,
+  external_urls: ExternalUrls,
+  href: string,
+  id: number,
+  images: Image[],
+  name: string,
+  owner: Owner,
+  public: boolean,
+  snapshot_id: string,
+  tracks: Tracks,
+  type: Type,
+  uri: string
+}
+
+export interface PlaylistsResponse {
+  href: string,
+  items: PlaylistItem[]
+}
+
+export type Playlists = PlaylistItem[] | null
+
+interface PlaylistState {
+  playlists: Playlists
+}
+
+const playlistItems = compose<PlaylistsResponse, Playlists, PlaylistState>(
+  assoc('playlists', __, {}),
+  prop('items')
+)
+
+export default null
+
+export const actions = {
+  getPlaylists: (_state: Store, authorization: string) => (
+    getPlaylists(authorization)
+      .map(playlistItems)
+      .promise()
+      .catch(console.error)
+  )
+}
