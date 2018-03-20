c113a093a4f50a87ea85c4d01cf315a8de570324
Thor Amorim <thoramorim@gmail.com>
2018-03-20 00:33:48 -0300

    Add store + router logic to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
+import Router from 'preact-router'
 import { h, render } from 'preact'
+import { Provider } from 'unistore/preact'
+
+import store from './store'
+import Redirect from './components/Redirect'
+import { actions } from './store/authentication'
 
 const container = document.getElementById('root') as HTMLElement
 
+const boundGetAuthentication = store.action(actions.getAuthentication)
+
+const onCallbackEnter = () =>
+  boundGetAuthentication(window.location.href)
+
 const renderApp = () => {
   const App = require('./components/App').default
-  render(<App />, container, container.lastChild as Element)
+  render(
+    <Provider store={store}>
+      <Router>
+        <Redirect
+          path="/callback"
+          to="/"
+          onEnter={onCallbackEnter}
+        />
+        <App default />
+      </Router>
+    </Provider>,
+    container,
+    container.lastChild as Element
+  )
 }
 
 if (module.hot) {
