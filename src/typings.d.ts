31d58ba9235ab5c18620118a066e1312d5434dea
Thor Amorim <thoramorim@gmail.com>
2018-03-20 00:37:29 -0300

    Fix unistore typings

---
+++ src/typings.d.ts
@@ -0,0 +1,39 @@
+declare module 'unistore' {
+  import {
+    Listener,
+    Unsubscribe,
+    Action,
+    BoundAction,
+    Store
+  } from 'unistore'
+  export default function createStore<K>(state?: K): Store<K>;
+  export type BoundAction = (...args: any[]) => void
+  export interface Store<K> {
+    action(action: Action<K>): BoundAction,
+    setState(update: object, overwrite?: boolean, action?: Action<K>): void,
+    subscribe(f: Listener<K>): Unsubscribe,
+    unsubscribe(f: Listener<K>): void,
+    getState(): K
+  }
+  export {
+    Listener,
+    Unsubscribe,
+    Action,
+    ActionFn,
+    ActionMap,
+    ActionCreator,
+    StateMapper,
+    connect,
+    ProviderProps,
+    Provider
+  } from 'unistore/index'
+}
+
+declare module 'unistore/devtools' {
+  import { Store } from 'unistore'
+  export function devtools<T>(store: Store<T>): Store<T>
+}
+
+declare module 'unistore/preact' {
+  export { connect, Provider } from 'unistore'
+}
