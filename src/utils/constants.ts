a9fcfbbaa9e63f9171aca0c2aad639f6119573af
Thor Amorim <thoramorim@gmail.com>
2018-03-24 04:24:08 -0300

    Add max limit on playlists URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 import { CLIENT_ID } from '../secrets'
 
 export const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=http://localhost:8080/callback`
-export const PLAYLISTS_URL = 'https://api.spotify.com/v1/me/playlists'
+export const PLAYLISTS_URL = 'https://api.spotify.com/v1/me/playlists?limit=50'
 export const USER_URL = 'https://api.spotify.com/v1/me'
